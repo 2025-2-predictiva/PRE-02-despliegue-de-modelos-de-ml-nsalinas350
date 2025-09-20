4df1822d887b73f531ff2dc711b9d7c5604447fd
nsalinas350 <nsalinas@unal.edu.co>
2025-09-20 15:59:42 -0500

    Taller repo 2

---
+++ homework/train_model.py
@@ -0,0 +1,14 @@
+import pickle 
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv("files/input/house_data.csv")
+
+features = df[["bedrooms", "bathrooms", "sqft_living", "sqft_lot", "floors", "waterfront", "condition"]]
+target = df["price"]
+
+estimator = LinearRegression()
+estimator.fit(features, target)
+
+with open("homework/house_predictor.pkl", "wb") as f:
+    pickle.dump(estimator, f)
